d7b88493d75cf54c21903d2bece80dad0f7d23fc
ofek pensso <ofekpenso@gmail.com>
2026-02-18 17:44:07 +0200

    add gitignore

---
+++ terraform/provider.tf
@@ -0,0 +1,19 @@
+# General
+.DS_Store
+*.log
+
+# Python / App
+__pycache__/
+*.py[cod]
+venv/
+.env
+
+# Terraform
+.terraform/
+*.tfstate
+*.tfstate.backup
+.terraform.lock.hcl
+
+# IDE
+.vscode/
+.idea/
